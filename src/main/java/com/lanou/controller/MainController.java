c4ce7eedb5fa3fe243d16c187c34fa4fa3fe9e05
748262420 <748262420@qq.com>
2017-08-15 19:36:32 +0800

    v2.0

--- src/main/java/com/lanou/controller/MainController.java
+++ src/main/java/com/lanou/controller/MainController.java
@@ -1,12 +1,17 @@
 package com.lanou.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lanou.bean.TbUser;
 import com.lanou.service.IUserService;
@@ -26,6 +31,19 @@ public class MainController {
 	@ResponseBody
 	public Map<String, Object> selectOne(TbUser user) {
 		return userservice.userLogin(user);
+	}
+
+	@RequestMapping(value = "/adddata", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> addData(MultipartFile file, HttpServletRequest request) throws IOException {
+		String path = request.getSession().getServletContext().getRealPath("upload");
+		String fileName = file.getOriginalFilename();
+		File dir = new File(path, fileName);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		file.transferTo(dir);
+		return null;
 	}
 
 }
